c45028062266bb2068f252fa1942e186854c1e31
Mehmet Bayram <akifbayram@gmail.com>
2024-11-17 16:31:29 -0500

    Update model directories

--- src/voronoi/setup.py
+++ src/voronoi/setup.py
@@ -4,6 +4,19 @@ import os
 
 package_name = 'voronoi'
 
+model_dirs = [
+    'turtlebot3_burger_0',
+    'turtlebot3_burger_1',
+    'turtlebot3_burger_2'
+]
+
+model_files = []
+for model_dir in model_dirs:
+    model_files.extend([
+        (os.path.join('share', package_name, 'models', model_dir), 
+         glob(os.path.join('models', model_dir, '*.*')))
+    ])
+
 setup(
     name=package_name,
     version='0.0.1',
@@ -15,12 +28,10 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # Include all launch files in the launch directory
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         (os.path.join('share', package_name, 'params'), glob('params/*.yaml')),
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, 'models'), glob('models/*/*.sdf')),
-    ],
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz'))
+    ] + model_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Akif Bayram',
@@ -30,7 +41,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'dijkstra = script.dijkstra:main', 
+            'dijkstra = script.dijkstra:main',
             'voronoi_tb4 = script.voronoi_tb4:main',
             'voronoi_tb3 = script.voronoi_tb3:main',
         ],
